f105c2fbd13b805d78351cf8104b23de320c1068
Ruslan Nevecherya <nevecherya@mlsdev.com>
2015-10-09 14:09:14 +0300

    update karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,30 @@
 module.exports = function (config) {
   config.set({
-    basePath: './source/',
+    basePath: './',
     frameworks: ['jasmine'],
+    browsers: ['PhantomJS'],
+    files: [
+      'node_modules/angular/angular.js',
+      'node_modules/angular-mocks/angular-mocks.js',
+      'sourse/ng-device.js',
+      'tests/*.js'
+    ],
+    plugins: [
+      'karma-jasmine',
+      'karma-nested-reporter',
+      'karma-phantomjs-launcher'
+    ],
+    reporters: ['nested'],
+      nestedReporter: {
+        color: {
+          should: 'red',
+          browser: 'yellow'
+        },
+        icon: {
+          failure: '✘ ',
+          indent: 'ட ',
+          browser: ''
+        }
+      }
   });
 };
