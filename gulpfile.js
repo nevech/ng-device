a5c902eb1f0b58eca2600288052ed9eab40f4cfa
Ruslan Nevecherya <nevecherya@mlsdev.com>
2015-10-09 13:29:10 +0300

    add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var ngAnnotate = require('gulp-ng-annotate');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+var karma = require('karma').server;
+
+gulp.task('build', function () {
+  gulp.src('source/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(ngAnnotate())
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(concat('ng-device.min.js'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('test', function (done) {
+  karma.start({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done);
+});
+
+gulp.task('watch', function () {
+  gulp.watch('source/*.js', ['test']);
+});
+
+gulp.task('default', ['test']);
