feffcfbd5815420dc75fe75430518a9c04c78732
nevech <r.nevech@gmail.com>
2015-10-09 15:15:25 +0300

    update tests

--- karma.conf.js
+++ karma.conf.js
@@ -6,7 +6,7 @@ module.exports = function (config) {
     files: [
       'node_modules/angular/angular.js',
       'node_modules/angular-mocks/angular-mocks.js',
-      'sourse/ng-device.js',
+      'source/*.js',
       'tests/*.js'
     ],
     plugins: [
--- tests/unit.js
+++ tests/unit.js
@@ -1,12 +1,32 @@
 describe("ng-device", function () {
-  var $device;
+  var deviceService;
 
-  beforeEach(function () {
-    module("ngDevice");
-  });
+  function loadModule(userAgent, vendor) {
+    module("ngDevice")
+    module(function ($deviceProvider) {
+      $deviceProvider.setUserAgent(userAgent);
+      $deviceProvider.setVendor(vendor);
+    });
+
+    inject(["$device", function ($device) {
+      deviceService = $device;
+    }]);
+  }
 
   it("should load", function () {
-    expect($device).not.toBeNull();
+    loadModule()
+    expect(deviceService).not.toBeNull();
   });
+
+  describe("User agent", function() {
+    it("should detect Google Chrome", function () {
+      var ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36";
+      var vendor = "Google Inc.";
+
+      loadModule(ua, vendor);
+      expect(deviceService.isChrome()).toBe(true);
+    });
+  });
+
 
 });
